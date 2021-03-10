b742c026962f38d1d5d79883cc2a467b8f2cee71
Fab4 <fablr@protonmail.com>
2021-03-10 18:41:55 +0100

    Ajout de la solution pour tower_builder.

---
+++ tower builder.py
@@ -0,0 +1,26 @@
+def tower_builder(n_floors):
+  n_floors += 1
+  result = []
+  for n in range(1, n_floors):
+    floor = ''
+    spaces = ''
+    stars = ''
+    
+    # Spaces
+    n_spaces = n_floors - n
+    for j in range(1, n_spaces):
+      spaces += ' '
+
+    # Stars
+    n_stars = 1 + (n - 1) * 2
+    for j in range(n_stars):
+      stars += '*'
+    
+    floor = spaces + stars + spaces
+    result.append(floor)
+      
+  return result
+
+print(tower_builder(1))
+print(tower_builder(2))
+print(tower_builder(3))
